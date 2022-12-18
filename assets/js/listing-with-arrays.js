9c68dc4ff48eebcfa0852fd92e2c7078fb92a0a1
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-18 18:01:31 +0400

    12/18 v2

---
+++ assets/js/listing-with-arrays.js
@@ -0,0 +1,9 @@
+
+const add = document.querySelector("#add");
+const sort = document.querySelector("#sort");
+const reverse = document.querySelector("#reverse");
+const removeL = document.querySelector("#removeL");
+const removeF = document.querySelector("#removeF");
+const removeA = document.querySelector("#removeA");
+const mix = document.querySelector("#mix");
+
