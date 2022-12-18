2efa40a8fe59cbd413da648379f408dac8de9c32
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-18 18:48:05 +0400

    12/18 v3

--- assets/js/listing-with-arrays.js
+++ assets/js/listing-with-arrays.js
@@ -1,9 +1,20 @@
 
+const input = document.querySelector("#input")
 const add = document.querySelector("#add");
+
 const sort = document.querySelector("#sort");
 const reverse = document.querySelector("#reverse");
 const removeL = document.querySelector("#removeL");
 const removeF = document.querySelector("#removeF");
 const removeA = document.querySelector("#removeA");
 const mix = document.querySelector("#mix");
+
+const list = document.querySelector(".listing");
+
+
+add.addEventListener("click", () => {
+    list.innerHTML = `<li>${input.value}</li>`
+})
+
+
 
