3ac3fa02ec0a4420c410b3b3e25965ebf14442db
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-19 18:56:52 +0400

    12/19 v2

--- assets/js/listing-with-arrays.js
+++ assets/js/listing-with-arrays.js
@@ -1,31 +1,40 @@
 
 const input = document.querySelector("#input")
-const add = document.querySelector("#add");
+const addBtn = document.querySelector("#add");
 
-const sort = document.querySelector("#sort");
+const sortBtn = document.querySelector("#sort");
 const reverse = document.querySelector("#reverse");
 const removeL = document.querySelector("#removeL");
 const removeF = document.querySelector("#removeF");
 const removeA = document.querySelector("#removeA");
-const mix = document.querySelector("#mix");
+const mixBtn = document.querySelector("#mix");
 
 const list = document.querySelector(".listing");
 
-
+listing = [];
 
 add.addEventListener("click", () => {
 
     !input.value && alert("Input cannot be blank");
 
-    element = `<li>${input.value}</li>`;
+    element = input.value;
 
-    list.innerHTML += element
+    listing.push(element)
+
+    for (const li of listing) {
+        list.innerHTML = `<li>${li}</li>`;;
+    }
 
     /* elements += `<li>${input.value}</li>`; */
 
 });
 
 sort.addEventListener("click", () => {
+    let lis = "";
+    for (const li of list) {
+        lis += li;
+    }
+    console.log(lis)
 });
 
 
