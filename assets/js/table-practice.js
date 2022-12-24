d2f34ddb6be499c01626287c24722420d2310c1b
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-24 18:23:03 +0400

    12/24 v2

--- assets/js/table-practice.js
+++ assets/js/table-practice.js
@@ -2,3 +2,10 @@
 const nameIn = document.querySelector("#name")
 const pointIn = document.querySelector("#point")
 const addBtn = document.querySelector("#addBtn")
+
+const addNew = () => {
+    let newName = nameIn.value;
+    let newPoint = pointIn.valu;
+
+    
+}
