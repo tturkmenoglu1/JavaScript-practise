6f97ece51efa46035c4e09aa19ecf7b5204dc590
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-26 13:11:39 +0400

    12/26 v3

--- assets/js/table-practice.js
+++ assets/js/table-practice.js
@@ -2,11 +2,11 @@
 const nameIn = document.querySelector("#name")
 const pointIn = document.querySelector("#point")
 const addBtn = document.querySelector("#addBtn")
-const tbody = document.querySelector("tbody")
+const tbody = document.querySelector(".tbody")
 
 addBtn.addEventListener("click", () => {
     let newName = nameIn.value;
-    let newPoint = pointIn.valu;
+    let newPoint = pointIn.value;
     let newStudent = `
     <tr>
      <th scope="row">1</th>
