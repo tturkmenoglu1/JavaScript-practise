8cdaabc903ebf0533b2dbd5356604063d7e346ad
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-12-27 16:27:15 +0400

    12/27 v2

--- assets/js/table-practice.js
+++ assets/js/table-practice.js
@@ -3,6 +3,9 @@ const nameIn = document.querySelector("#name")
 const pointIn = document.querySelector("#point")
 const addBtn = document.querySelector("#addBtn")
 const tbody = document.querySelector(".tbody")
+const delBtn = document.querySelectorAll(".delete")
+
+const scores = document.querySelectorAll(".pointV")
 
 const idGen = () => {
   
@@ -26,3 +29,35 @@ addBtn.addEventListener("click", () => {
     
     tbody.innerHTML += newStudent
 })
+
+
+
+/* delBtn.addEventListener("click", (e) => {
+  alert(ok)
+  const row = e.target.closest("tr")
+
+  const result = confirm(`Are you sure to delete ${row}?`);
+  if (result) {
+    row = "";
+  }
+}) */
+
+const findAvrg = () => {
+  let temp = 0;
+  let len = scores.length
+
+  scores.forEach(score => {
+    console.log(score.innerText)
+    temp += score.innerText;
+    console.log(temp)
+  });
+
+  console.log("temp / len " + temp / len)
+  let result = Number(temp / len);
+  const average = document.querySelector("#average");
+
+  average.innerHTML = result
+  console.log("result " + result)
+}
+
+findAvrg();
